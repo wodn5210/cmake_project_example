a46f2e70def74a7a59d89a20234aa8ab733a7fbf
wodn5210 <wodn5210@gmail.com>
2023-07-18 23:49:38 +0900

    glfw 윈도우 띄우기

--- Dependency.cmake
+++ Dependency.cmake
@@ -9,7 +9,7 @@ set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 
 # spdlog: fast logger library
 ExternalProject_Add(                                                # git 에서 다운받은다음 cmake를 실행해준다.
-    dep-spdlog                                                  #프로젝트의 대표이름
+    dep_spdlog                                                  #프로젝트의 대표이름
     GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
     GIT_TAG "v1.x"                                              #git 페이지에서 branch 버전이름이나 태그를 설정해주면된다.
     GIT_SHALLOW 1                                               #shallow 활성화(가장최신의 버전만 다운받는다.)
@@ -20,6 +20,25 @@ ExternalProject_Add(                                                # git 에서
 )
 
 # Dependency 리스트 및 라이브러리 파일 리스트 추가
-set(DEP_LIST ${DEP_LIST} dep-spdlog)
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
 set(DEP_LIBS ${DEP_LIBS} spdlog)
 #set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)           # 이거 안됨 ㅜ
+
+
+# # glfw
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.8"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" 
+    PATCH_COMMAND "" 
+    TEST_COMMAND ""
+    CMAKE_ARGS                                          # openglfw 에서 사용할 옵션들 - 해당 makefile에 보면 있음
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}       # speed log와 옵션 동일
+        -DGLFW_BUILD_EXAMPLES=OFF                       # 예제, 테트트파일, 문서
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+    )
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
